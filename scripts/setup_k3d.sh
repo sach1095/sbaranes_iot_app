8635e38f09e390474dad082631e06cdc68c76bb5
root <root@vmIot.myguest.virtualbox.org>
2024-03-05 09:31:59 +0100

    add script

--- scripts/setup_k3d.sh
+++ scripts/setup_k3d.sh
@@ -9,3 +9,8 @@ kubectl create namespace argocd
 
 echo 'Installing Argo CD'
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+
+echo 'Deploy App'
+kubectl apply -f ../app-p3-deployment.yaml
+kubectl apply -f app-p3-service.yaml
+kubectl apply -f ingress.yaml
